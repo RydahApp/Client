6a1be867be505bee26809c49210bf025d2a43b7a
Abdulrauf Lukman <AbdulraufLukman@AbulAswads-MacBook-Pro.local>
2024-03-13 02:29:40 -0700

    create the password success page

--- src/screens/auth/setSuccess.tsx
+++ src/screens/auth/setSuccess.tsx
@@ -1,15 +1,42 @@
+import { hp } from "@/utils/dimensions";
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
-import Icon from "react-native-vector-icons/MaterialIcons";
+import { View, Text, StyleSheet, Image } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import Icon from "react-native-vector-icons/AntDesign";
 
-export default function SetSuccess() {
+export default function SetSuccessPage() {
   return (
-    <View>
-      <Text>Password reset successful </Text>
-      <TouchableOpacity>
-        <Text>Proceed to Login</Text>
-        <Icon name="error-outline" size={30} color="#CB1A14" />
+    <View style={styles.container}>
+      <View style={{ gap: hp(15), marginTop: hp(200) }}>
+        <Text
+          style={{ fontWeight: "400", fontSize: hp(16), textAlign: "center" }}
+        >
+          Password reset successful{" "}
+        </Text>
+      </View>
+      <TouchableOpacity style={styles.button}>
+        <Text style={{ fontWeight: "600" }}>Proceed to Login</Text>
+        <Icon name="arrowright" size={15} />
       </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: hp(60),
+  },
+  button: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: hp(8),
+    backgroundColor: "#FFCCCC",
+    padding: hp(10),
+    paddingHorizontal: hp(15),
+    borderRadius: hp(8),
+    marginTop: hp(10),
+  },
+});
