33f86746635611d00456eddea443070beb622e2e
Efe G <98314362+gift56@users.noreply.github.com>
2024-07-15 03:47:11 -0700

    done working on verify page

--- app/(auth)/forgetpassword.tsx
+++ app/(auth)/forgetpassword.tsx
@@ -5,27 +5,26 @@ import { ScrollView } from "react-native";
 import { CustomButton, FormField, Topheader } from "@/components";
 import { Image } from "react-native";
 import { icons } from "@/constants";
-import { authFormSchema } from "@/schema";
+import { forgetSchema } from "@/schema";
 import { useFormik } from "formik";
 import { router } from "expo-router";
-import { authFormValueType } from "@/types";
+import { forgetFormValueType } from "@/types";
 import Toast from "react-native-toast-message";
+import { Entypo } from "@expo/vector-icons";
 
 const ForgetPasswordScreen = () => {
-  const initialValues: authFormValueType = {
+  const initialValues: forgetFormValueType = {
     email: "",
-    password: "",
   };
 
-  const onSubmit = (payload: authFormValueType, action: any) => {
+  const onSubmit = (payload: forgetFormValueType, action: any) => {
     console.log(payload);
     Toast.show({
       type: "success",
       text1: "We’ve sent you 4-digit code to your Mail",
       position: "bottom",
     });
-    router.push(`/verifyemail/${payload.email}`);
-
+    router.push(`/resetpassword/otp/${payload.email}`);
     action.resetForm();
   };
 
@@ -39,13 +38,69 @@ const ForgetPasswordScreen = () => {
     handleSubmit,
   } = useFormik({
     initialValues,
-    validationSchema: authFormSchema,
+    validationSchema: forgetSchema,
     onSubmit,
   });
+
   return (
-    <View>
-      <Text>ForgetPasswordScreen</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white">
+      <ScrollView
+        contentContainerStyle={{ paddingHorizontal: 20, paddingVertical: 50 }}
+      >
+        <View className="w-full flex-col items-start justify-start space-y-4">
+          <TouchableOpacity onPress={() => router.back()} className="mb-10">
+            <Entypo name="chevron-thin-left" size={24} color="black" />
+          </TouchableOpacity>
+          <Topheader
+            subtitle="Ready to gain access back?"
+            title="Reset password"
+          />
+          <Text className="text-sm font-normal text-[#997A7A] pt-6">
+            Enter your email address below, and we'll send you a link to reset
+            your password. Once you've received it, simply follow the
+            instructions in the email to set up a new password and regain access
+            to your account.
+          </Text>
+          <View className="w-full flex-col space-y-2 items-start justify-start">
+            <View className="relative w-full">
+              <FormField
+                title={
+                  <Text className="text-sm font-medium text-black">Email</Text>
+                }
+                titleShow={true}
+                value={values.email}
+                handleChangeText={handleChange("email")}
+                onBlur={handleBlur("email")}
+                otherStyles="mt-4"
+                placeholder="Input email address"
+                keyboardType="email-address"
+                errorClass={`${
+                  touched.email && errors.email ? "!border-red-500" : ""
+                } pl-[45px]`}
+              />
+              <View className="flex-row items-center justify-start space-x-2 absolute top-[66%] left-[6%]">
+                <Image
+                  source={icons.mailIcon}
+                  resizeMode="contain"
+                  className="w-4 h-4"
+                />
+              </View>
+            </View>
+            {touched.email && errors.email ? (
+              <Text style={{ color: "red" }}>{errors.email}</Text>
+            ) : null}
+
+            <CustomButton
+              title="Reset password"
+              containerStyles="bg-primary my-8 w-full py-4"
+              titleStyle="text-base font-medium text-black"
+              isLoading={isSubmitting}
+              handlePress={handleSubmit}
+            />
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
