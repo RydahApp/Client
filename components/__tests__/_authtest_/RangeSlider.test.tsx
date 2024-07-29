69a4aa4a5acdf39bb0fe741f6bd849fd1c306f71
Efe G <98314362+gift56@users.noreply.github.com>
2024-07-29 09:43:33 -0700

    done witn Range Slider test

--- components/__tests__/_authtest_/RangeSlider.test.tsx
+++ components/__tests__/_authtest_/RangeSlider.test.tsx
@@ -1,16 +1,15 @@
-// RangeSlider.test.tsx
-import React from 'react';
-import { render, fireEvent } from '@testing-library/react-native';
-import RangeSlider from '@/components/auth/RangeSlider';
+import React from "react";
+import { render } from "@testing-library/react-native";
+import RangeSlider from "@/components/auth/RangeSlider";
 
-describe('RangeSlider', () => {
+describe("RangeSlider", () => {
   const sliderWidth = 300;
   const min = 0;
   const max = 100;
   const step = 1;
   const onValueChange = jest.fn();
 
-  it('renders correctly', () => {
+  it("renders correctly", () => {
     const { getByTestId } = render(
       <RangeSlider
         sliderWidth={sliderWidth}
@@ -20,52 +19,8 @@ describe('RangeSlider', () => {
         onValueChange={onValueChange}
       />
     );
-    expect(getByTestId('sliderContainer')).toBeTruthy();
-    expect(getByTestId('thumb1')).toBeTruthy();
-    expect(getByTestId('thumb2')).toBeTruthy();
+    expect(getByTestId("sliderContainer")).toBeTruthy();
+    expect(getByTestId("thumb1")).toBeTruthy();
+    expect(getByTestId("thumb2")).toBeTruthy();
   });
-
-  // it('calls onValueChange when thumbs are moved', () => {
-  //   const { getByTestId } = render(
-  //     <RangeSlider
-  //       sliderWidth={sliderWidth}
-  //       min={min}
-  //       max={max}
-  //       step={step}
-  //       onValueChange={onValueChange}
-  //     />
-  //   );
-
-  //   const thumb1 = getByTestId('thumb1');
-  //   const thumb2 = getByTestId('thumb2');
-
-  //   // Simulate thumb movements
-  //   fireEvent(thumb1, 'onGestureEvent', {
-  //     nativeEvent: {
-  //       translationX: 50,
-  //     },
-  //   });
-  //   fireEvent(thumb1, 'onHandlerStateChange', {
-  //     nativeEvent: {
-  //       state: 5, // End state
-  //     },
-  //   });
-
-  //   fireEvent(thumb2, 'onGestureEvent', {
-  //     nativeEvent: {
-  //       translationX: -50,
-  //     },
-  //   });
-  //   fireEvent(thumb2, 'onHandlerStateChange', {
-  //     nativeEvent: {
-  //       state: 5, // End state
-  //     },
-  //   });
-
-  //   expect(onValueChange).toHaveBeenCalledTimes(2);
-  //   expect(onValueChange).toHaveBeenCalledWith({
-  //     min: expect.any(Number),
-  //     max: expect.any(Number),
-  //   });
-  // });
 });
