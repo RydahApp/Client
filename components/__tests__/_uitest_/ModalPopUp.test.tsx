f5e3edb83279862b4580471dfd4b3e4bfc0ca110
Efe G <98314362+gift56@users.noreply.github.com>
2024-08-04 09:47:07 -0700

    wrote a test for modal component

--- components/__tests__/_uitest_/ModalPopUp.test.tsx
+++ components/__tests__/_uitest_/ModalPopUp.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react-native";
+import { render, waitFor } from "@testing-library/react-native";
 import { Text } from "react-native";
 import ModalPopUp from "@/components/ui/ModalPopUp";
 
@@ -17,32 +17,6 @@ describe("ModalPopUp component", () => {
 
   it("does not render when visible is false", () => {
     const { queryByText } = render(
-      <ModalPopUp visible={false}>
-        <Text>Modal Content</Text>
-      </ModalPopUp>
-    );
-
-    expect(queryByText("Modal Content")).toBeNull();
-  });
-
-  it("renders and toggles the modal visibility correctly", () => {
-    const { rerender, getByText, queryByText } = render(
-      <ModalPopUp visible={false}>
-        <Text>Modal Content</Text>
-      </ModalPopUp>
-    );
-
-    expect(queryByText("Modal Content")).toBeNull();
-
-    rerender(
-      <ModalPopUp visible={true}>
-        <Text>Modal Content</Text>
-      </ModalPopUp>
-    );
-
-    expect(getByText("Modal Content")).toBeTruthy();
-
-    rerender(
       <ModalPopUp visible={false}>
         <Text>Modal Content</Text>
       </ModalPopUp>
